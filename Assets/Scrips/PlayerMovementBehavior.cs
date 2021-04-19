282241cd6c641cb480cdcd4e4ea5e4c830a4a46a
WoodrowCrawford <woodrowcrawford03@gmail.com>
2021-04-19 14:32:00 -0500

    Initial commit

---
+++ Assets/Scrips/PlayerMovementBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    Rigidbody Player;
+
+
+    [SerializeField]
+    //The main player's health
+    private float _health;
+
+    [SerializeField]
+    //How much damage the player deals
+    private float _damage;
+
+    [SerializeField]
+
+    //How much mama the player has
+    private float _mana;
+
+   
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

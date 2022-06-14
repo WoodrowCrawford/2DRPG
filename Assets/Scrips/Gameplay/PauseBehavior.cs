ccfb4156785882d822a162861694dfa1264b5bc5
WoodrowCrawford <woodrowcrawford03@gmail.com>
2022-06-14 09:15:42 -0500

    Fine tuned some bugs; Added a way to reset the game

---
+++ Assets/Scrips/Gameplay/PauseBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseBehavior : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Resets the game when the "R" key is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+}
